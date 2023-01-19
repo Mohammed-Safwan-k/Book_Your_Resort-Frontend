d8a242d79e5b0619c0c8cb0a1dec84ef7afc0073
Mohammed Safwan <safwanpklr@gmail.com>
2023-01-19 16:25:58 +0530

    started

--- src/scenes/loginPage/index.jsx
+++ src/scenes/loginPage/index.jsx
@@ -1,4 +1,5 @@
 import { Box, Typography, useTheme, useMediaQuery } from "@mui/material";
+import Form from "./Form"
 
 const LoginPage = () => {
   const theme = useTheme();
@@ -16,7 +17,18 @@ const LoginPage = () => {
           Weirdopedia
         </Typography>
       </Box>
-      
+      <Box
+        width={isNonMobileScreens ? "50%" : "93%"}
+        p="2rem"
+        m="2rem auto"
+        borderRadius="1.5rem"
+        backgroundColor={theme.palette.background.alt}
+      >
+        <Typography fontWeight="500" variant="h5" sx={{ mb: "1.5rem" }}>
+          Welcome to Weirdopedia, the place for Weirdoo's !
+        </Typography>
+        
+      </Box>
     </Box>
   );
 };
